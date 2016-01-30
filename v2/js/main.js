574acee02ab31b220f0ed37be4ad920912516e9f
smartm13 <smartm13@gmail.com>
2016-01-30 22:45:16 +0530

    try set 2
    from push-notifications-master
    this time concrete one.

---
+++ v2/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// TODO
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('sw.js').then(function(reg) {
+        console.log(':^)', reg);
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+			document.getElementById("url").innerHTML=sub.endpoint;
+			//alert(sub.endpoint);
+        });
+    }).catch(function(error) {
+        console.log(':^(', error);
+    });
+}
