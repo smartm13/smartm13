a93fed20c82d9257669a11d65938729c46e795be
smartm13 <smartm13@gmail.com>
2016-01-31 14:43:39 +0530

    jus go

--- v2/sw.js
+++ v2/sw.js
@@ -24,6 +24,18 @@
 // TODO
 function httpGet(theUrl)
 {
+if (typeof XMLHttpRequest === "undefined") {
+  XMLHttpRequest = function () {
+    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+    catch (e) {}
+    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+    catch (e) {}
+    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+    catch (e) {}
+    throw new Error("This browser does not support XMLHttpRequest.");
+  };
+}
+
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
